9975086d321911cf40127bd4afe62e41d54b903e
Eingeniator <jeanlucmantoine@gmail.com>
2026-03-18 02:21:36 -0400

    Feat(ai): Fixed difficulty ranges bug

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def get_range_for_difficulty(difficulty: str):
     if difficulty == "Normal":
         return 1, 100
     if difficulty == "Hard":
-        return 1, 50
+        return 1, 500
     return 1, 100
 
 
